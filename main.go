7293701af8d3c0f2290478afe9e1dd56fde47d2b
zxnui <zhuxunui@gmail.com>
2017-05-08 15:24:28 +0800

    新链接

--- main.go
+++ main.go
@@ -6,16 +6,6 @@ import (
 )
 
 func main() {
-
-	////若是登录过，则前端显示用户名
-	//var finish = func(ctx *context.Context) {
-	//	_, ok := ctx.
-	//	if !ok && ctx.Request.RequestURI != "/login" {
-	//		ctx.Redirect(302, "/login")
-	//	}
-	//}
-	//
-	//beego.InsertFilter("*",beego.FinishRouter,finish)
 	beego.Run()
 }
 
