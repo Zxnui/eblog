d08db06c8619007ba47d89ee836478a0f831e337
zxnui <zhuxunui@163.com>
2017-11-24 16:43:25 +0800

    一些更新

---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+$(function () {
+    console.log("联系方式:");
+    console.log("   zhuxunui@163.com");
+    console.log("   zhuxunui@gmail.com");
+    console.log("github:");
+    console.log("   github.com/zxnui");
+});
+
